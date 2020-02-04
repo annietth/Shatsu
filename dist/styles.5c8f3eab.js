eb71be92b079870310570e77e1b2dd5da2e64df7
tania-abitbol <56915461+tania-abitbol@users.noreply.github.com>
2020-02-04 09:41:30 +0100

    all bug

--- dist/styles.5c8f3eab.js
+++ dist/styles.5c8f3eab.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"/Users/tania/Shatsu/src/img/backproduct.png":[["backproduct.1bc54ddd.png","img/backproduct.png"],"img/backproduct.png"],"/Users/tania/Shatsu/src/img/backproductflip.png":[["backproductflip.7ea28544.png","img/backproductflip.png"],"img/backproductflip.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61221" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49422" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
